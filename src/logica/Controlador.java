a8f507e46768ceb6fa04ee143a98834e4d6a7bee
Alberto <agonzalezbermejo13@gmail.com>
2020-11-24 19:31:30 +0100

    creacion de controlador

---
+++ src/logica/Controlador.java
@@ -0,0 +1,24 @@
+package logica;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import vista.Inicio;
+
+public class Controlador implements ActionListener {
+	private Inicio inicio;
+
+	public static void main(String[] args) {
+		
+
+		
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
