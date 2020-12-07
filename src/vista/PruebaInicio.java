79da87ba0a95249aaeb80c28b5b96c4d3ee16512
Alberto <agonzalezbermejo13@gmail.com>
2020-12-08 00:47:45 +0100

    Cambios en la vista de Prueba Inicio

--- src/vista/PruebaInicio.java
+++ src/vista/PruebaInicio.java
@@ -18,6 +18,7 @@ import javax.swing.JSeparator;
 import javax.swing.JTextArea;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.CompoundBorder;
+import javax.swing.SwingConstants;
 
 public class PruebaInicio extends JFrame {
 
@@ -30,7 +31,7 @@ public class PruebaInicio extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					PruebaEscuderia frame = new PruebaEscuderia();
+					PruebaInicio frame = new PruebaInicio();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -43,11 +44,6 @@ public class PruebaInicio extends JFrame {
 	 * Create the frame.
 	 */
 	public PruebaInicio() {
-		Toolkit pantalla=Toolkit.getDefaultToolkit();
-		Dimension  tamano=pantalla.getScreenSize();
-		
-		
-		int alto=(int)tamano.height+100,ancho=(int)tamano.width+2000;
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(0, 0, 990,650);
@@ -116,76 +112,79 @@ public class PruebaInicio extends JFrame {
 		btnSimulacion.setBounds(747, 11, 115, 48);
 		PanelMenu.add(btnSimulacion);
 		
-		JPanel panel = new JPanel();
-		panel.setBounds(0, 72, 974, 539);
-		contentPane.add(panel);
-		panel.setLayout(null);
+		JPanel panelInicio = new JPanel();
+		panelInicio.setBounds(0, 72, 974, 539);
+		contentPane.add(panelInicio);
+		panelInicio.setLayout(null);
 		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		panel_1.setBounds(27, 25, 328, 285);
-		panel.add(panel_1);
-		panel_1.setLayout(null);
+		JPanel panelNoticia1 = new JPanel();
+		panelNoticia1.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		panelNoticia1.setBounds(27, 11, 328, 285);
+		panelInicio.add(panelNoticia1);
+		panelNoticia1.setLayout(null);
 		
 		JTextArea txtrBottaslosQue = new JTextArea();
+		txtrBottaslosQue.setEditable(false);
 		txtrBottaslosQue.setFont(new Font("Consolas", Font.BOLD, 15));
 		txtrBottaslosQue.setOpaque(false);
 		txtrBottaslosQue.setLineWrap(true);
 		txtrBottaslosQue.setWrapStyleWord(true);
 		txtrBottaslosQue.setText("Bottas: \"Los que no saben de F1 podr\u00EDan pensar que he quedado como un completo idiota\"");
 		txtrBottaslosQue.setBounds(31, 175, 270, 61);
-		panel_1.add(txtrBottaslosQue);
+		panelNoticia1.add(txtrBottaslosQue);
 		
 		JLabel lblNewLabel = new JLabel("");
 		lblNewLabel.setIcon(new ImageIcon(PruebaInicio.class.getResource("/Imagenes/Noticias/Noticia.jpg")));
 		lblNewLabel.setBounds(31, 21, 270, 144);
-		panel_1.add(lblNewLabel);
+		panelNoticia1.add(lblNewLabel);
 		
 		JButton btnLeerMas1 = new JButton("Leer m\u00E1s");
 		btnLeerMas1.setBorder(new CompoundBorder());
 		btnLeerMas1.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		btnLeerMas1.setBounds(223, 246, 95, 29);
-		panel_1.add(btnLeerMas1);
+		panelNoticia1.add(btnLeerMas1);
 		
-		JPanel panel_1_1 = new JPanel();
-		panel_1_1.setLayout(null);
-		panel_1_1.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		panel_1_1.setBounds(384, 25, 319, 387);
-		panel.add(panel_1_1);
+		JPanel panelNoticia2 = new JPanel();
+		panelNoticia2.setLayout(null);
+		panelNoticia2.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		panelNoticia2.setBounds(370, 11, 319, 387);
+		panelInicio.add(panelNoticia2);
 		
 		JTextArea txtrMclarenReafirmaSu = new JTextArea();
+		txtrMclarenReafirmaSu.setEditable(false);
 		txtrMclarenReafirmaSu.setWrapStyleWord(true);
 		txtrMclarenReafirmaSu.setText("McLaren reafirma su estrategia");
 		txtrMclarenReafirmaSu.setOpaque(false);
 		txtrMclarenReafirmaSu.setFont(new Font("Consolas", Font.BOLD, 17));
 		txtrMclarenReafirmaSu.setBounds(27, 207, 293, 25);
-		panel_1_1.add(txtrMclarenReafirmaSu);
+		panelNoticia2.add(txtrMclarenReafirmaSu);
 		
 		JLabel lblNewLabel_1 = new JLabel("");
 		lblNewLabel_1.setIcon(new ImageIcon(PruebaInicio.class.getResource("/Imagenes/Noticias/Noticia2.jpg")));
 		lblNewLabel_1.setBounds(27, 26, 270, 152);
-		panel_1_1.add(lblNewLabel_1);
+		panelNoticia2.add(lblNewLabel_1);
 		
 		JButton btnLeerMas2 = new JButton("Leer m\u00E1s");
 		btnLeerMas2.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		btnLeerMas2.setBorder(new CompoundBorder());
-		btnLeerMas2.setBounds(223, 348, 95, 29);
-		panel_1_1.add(btnLeerMas2);
+		btnLeerMas2.setBounds(214, 347, 95, 29);
+		panelNoticia2.add(btnLeerMas2);
 		
 		JTextArea txtrSeidlApoyaLa = new JTextArea();
+		txtrSeidlApoyaLa.setEditable(false);
 		txtrSeidlApoyaLa.setWrapStyleWord(true);
 		txtrSeidlApoyaLa.setText("Seidl apoya la doble parada en Sakhir que dej\u00F3 a Sainz cuarto, a un paso del podio. \"Checo estaba a otro nivel\", dice el jefe a AS. El MCL35 no puede con Racing Point.");
 		txtrSeidlApoyaLa.setOpaque(false);
 		txtrSeidlApoyaLa.setLineWrap(true);
 		txtrSeidlApoyaLa.setFont(new Font("Consolas", Font.PLAIN, 15));
 		txtrSeidlApoyaLa.setBounds(27, 234, 270, 118);
-		panel_1_1.add(txtrSeidlApoyaLa);
+		panelNoticia2.add(txtrSeidlApoyaLa);
 		
-		JPanel panel_1_1_1 = new JPanel();
-		panel_1_1_1.setLayout(null);
-		panel_1_1_1.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		panel_1_1_1.setBounds(37, 326, 328, 203);
-		panel.add(panel_1_1_1);
+		JPanel panelNoticia3 = new JPanel();
+		panelNoticia3.setLayout(null);
+		panelNoticia3.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		panelNoticia3.setBounds(27, 307, 328, 203);
+		panelInicio.add(panelNoticia3);
 		
 		JTextArea txtrMclarenReafirmaSu_1 = new JTextArea();
 		txtrMclarenReafirmaSu_1.setWrapStyleWord(true);
@@ -193,17 +192,17 @@ public class PruebaInicio extends JFrame {
 		txtrMclarenReafirmaSu_1.setOpaque(false);
 		txtrMclarenReafirmaSu_1.setFont(new Font("Consolas", Font.BOLD, 17));
 		txtrMclarenReafirmaSu_1.setBounds(27, 207, 293, 25);
-		panel_1_1_1.add(txtrMclarenReafirmaSu_1);
+		panelNoticia3.add(txtrMclarenReafirmaSu_1);
 		
 		JLabel lblNewLabel_1_1 = new JLabel("");
 		lblNewLabel_1_1.setBounds(10, 10, 270, 152);
-		panel_1_1_1.add(lblNewLabel_1_1);
+		panelNoticia3.add(lblNewLabel_1_1);
 		
 		JButton btnLeerMas2_1 = new JButton("Leer m\u00E1s");
 		btnLeerMas2_1.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		btnLeerMas2_1.setBorder(new CompoundBorder());
 		btnLeerMas2_1.setBounds(223, 348, 95, 29);
-		panel_1_1_1.add(btnLeerMas2_1);
+		panelNoticia3.add(btnLeerMas2_1);
 		
 		JTextArea txtrSeidlApoyaLa_1 = new JTextArea();
 		txtrSeidlApoyaLa_1.setWrapStyleWord(true);
@@ -212,20 +211,45 @@ public class PruebaInicio extends JFrame {
 		txtrSeidlApoyaLa_1.setLineWrap(true);
 		txtrSeidlApoyaLa_1.setFont(new Font("Consolas", Font.PLAIN, 15));
 		txtrSeidlApoyaLa_1.setBounds(27, 234, 270, 118);
-		panel_1_1_1.add(txtrSeidlApoyaLa_1);
+		panelNoticia3.add(txtrSeidlApoyaLa_1);
 		
-		JLabel lblNewLabel_2 = new JLabel("");
-		lblNewLabel_2.setIcon(new ImageIcon(PruebaInicio.class.getResource("/Imagenes/Noticias/Circuito.jpg")));
-		lblNewLabel_2.setBounds(713, 137, 238, 203);
+		JPanel panel = new JPanel();
+		panel.setBounds(699, 11, 265, 332);
+		panelInicio.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblGP = new JLabel("");
+		lblGP.setIcon(new ImageIcon("E:\\Users\\Usuaio\\Interfaces\\TrabajoFormula1\\src\\Imagenes\\Grandes Premios\\Portada\\Abudhabi.png"));
+		lblGP.setBounds(10, 11, 245, 180);
+		panel.add(lblGP);
+		
+		JPanel pC3 = new JPanel();
+		pC3.setBackground(new Color(255, 159, 0));
+		pC3.setBounds(10, 202, 15, 17);
+		panel.add(pC3);
+		
+		JLabel lblTitulo = new JLabel("PR\u00D3XIMO GP DE F\u00D3RMULA 1");
+		lblTitulo.setBounds(35, 202, 220, 17);
+		panel.add(lblTitulo);
+		
+		JLabel lblNewLabel_2 = new JLabel("GP DE ABU DHABI");
+		lblNewLabel_2.setFont(new Font("Bauhaus 93", Font.PLAIN, 25));
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_2.setBounds(10, 245, 245, 103);
 		panel.add(lblNewLabel_2);
 		
-		JTextArea txtrFormulaDhl = new JTextArea();
-		txtrFormulaDhl.setOpaque(false);
-		txtrFormulaDhl.setWrapStyleWord(true);
-		txtrFormulaDhl.setLineWrap(true);
-		txtrFormulaDhl.setFont(new Font("Bauhaus 93", Font.PLAIN, 18));
-		txtrFormulaDhl.setText("Formula 1 DHL Turkish Grand Prix 2020");
-		txtrFormulaDhl.setBounds(713, 350, 238, 62);
-		panel.add(txtrFormulaDhl);
+		JSeparator separator = new JSeparator();
+		separator.setBounds(10, 311, 245, 10);
+		panel.add(separator);
+		
+		JLabel lblFecha = new JLabel("13 DE DICIEMBRE");
+		lblFecha.setHorizontalAlignment(SwingConstants.CENTER);
+		lblFecha.setBounds(57, 230, 116, 14);
+		panel.add(lblFecha);
+		
+		JLabel lblFondo = new JLabel();
+		lblFondo.setBounds(0, 0, 974, 539);
+		panelInicio.add(lblFondo);
+		lblFondo.setIcon(new ImageIcon("src"+File.separator+"Imagenes"+File.separator+"Fondos"+File.separator+"Fondo6.PNG"));
 	}
 }
