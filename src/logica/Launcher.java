1d125dfc4d6e40c0825032a3038796fb729ca708
Ruben <rubengulp21@gmail.com>
2020-12-09 00:44:02 +0100

    ttt

--- src/logica/Launcher.java
+++ src/logica/Launcher.java
@@ -14,39 +14,6 @@ public class Launcher {
 		
 		c.getInicio().inicio(c);
 		c.getInicio().setVisible(true);
-//		ConectionHelper connection = new ConectionHelper();
-//		Connection c=null;
-//		Piloto p=null;
-//		Escuderia es=null;
-//		try {
-//			p=connection.consultaPiloto("Lewis Hamilton");
-//			System.out.println(p.toString());
-//			
-//			es=connection.selectEscuderia("Mercedes AMG Petronas F1 TEAM");
-//			System.out.println(es.toString());
-//			
-//		} catch (ClassNotFoundException |SQLException e) {
-//			e.printStackTrace();
-//		} finally {
-//			connection.disconnect(c);
-//		}
-//	double pilotoHabulydad=	(Math.random()*80+1);
-//	double diestroCurvoso=5;
-//	double borracho =-5;
-//	double piloto2=(Math.random()*50+1)+diestroCurvoso;
-//	
-//	ArrayList<Double> cc =new ArrayList<Double>();
-//	cc.add(pilotoHabulydad);
-//	cc.add(piloto2);
-//	if (piloto2>pilotoHabulydad) {
-//		System.out.println("piloto2 adelanta");
-//		
-//	}
-//	Collections.sort(cc, Collections.reverseOrder());
-//	
-//	
-//		System.out.println(cc);
-//	
 //	
 	}
 	
